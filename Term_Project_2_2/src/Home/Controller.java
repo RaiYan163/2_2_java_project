19d4ebf7df3179bfc81f2bf48df636d2fec6ecb8
RaiYan_163 <raiyanashraf20@gmail.com>
2023-03-09 00:15:52 +0600

    StudBud version 1.0

---
+++ Term_Project_2_2/src/Home/Controller.java
@@ -0,0 +1,19 @@
+package Home;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class Controller {
+    @FXML
+    private Button logout;
+
+
+    public void userLogOut(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("sample.fxml");
+
+    }
+}
